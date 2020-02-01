f7ddc6e2ebe321b9c2bbb6347e09a840981769b1
Robert Papoyan <robertpapoyan96@gmail.com>
2020-02-01 23:12:10 +0400

    Fixed Linear Layout in League layout, and toggled button checked and unchecked functions added/ From Home

--- app/src/main/java/com/example/swooshapp/LeaugeActivity.kt
+++ app/src/main/java/com/example/swooshapp/LeaugeActivity.kt
@@ -2,11 +2,39 @@ package com.example.swooshapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_leauge.*
 
 class LeaugeActivity : AppCompatActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leauge)
+    }
+
+    fun onMenClicked(view: View){
+
+        toggleWomenBtn.isChecked = false
+        toggleCoEdBtn.isChecked = false
+
+        selectedLeague = "men"
+    }
+
+    fun onWomenClicked(view: View){
+
+        toggleMenBtn.isChecked = false
+        toggleCoEdBtn.isChecked = false
+
+        selectedLeague = "women"
+    }
+
+    fun onCoEdClicked(view: View){
+
+        toggleMenBtn.isChecked = false
+        toggleWomenBtn.isChecked = false
+
+        selectedLeague = "co ed"
     }
 }
