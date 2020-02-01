02c7f3be88f82db47d1e39fcdb39e916ebf2313a
Robert Papoyan <robertpapoyan96@gmail.com>
2020-02-01 18:33:24 +0400

    First And Second Layouts: Home

---
+++ app/src/main/java/com/example/swooshapp/LeaugeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swooshapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaugeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leauge)
+    }
+}
